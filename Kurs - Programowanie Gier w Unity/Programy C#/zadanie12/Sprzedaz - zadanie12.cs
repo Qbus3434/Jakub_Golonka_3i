eabe6a4493e9a903b12ef702a89cea1913062727
JGolonka <90254629+Qbus3434@users.noreply.github.com>
2021-11-12 10:50:30 +0100

    Add files via upload

---
+++ Kurs - Programowanie Gier w Unity/Programy C#/zadanie12/Sprzedaz - zadanie12.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace zadanie12
+{
+    public class Sprzedaz
+    {
+        #region PolaKlasy
+        public string nazwaProduktu;
+        public int ilosc;
+        public decimal cena;
+        #endregion
+
+        #region Konstruktor
+        public Sprzedaz(string nazwaProduktu, int ilosc, decimal cena)
+        {
+            this.nazwaProduktu = nazwaProduktu;
+            this.ilosc = ilosc;
+            this.cena = cena;
+        }
+
+
+        public void wypisz()
+        {
+            Console.WriteLine($"Produkt o nazwie {nazwaProduktu} w ilości {ilosc} w cenie {cena} za sztukę.");
+        }
+        #endregion
+    }
+}
---
+++ Kurs - Programowanie Gier w Unity/Programy C#/zadanie12/Zamowienie - zadanie12.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace zadanie12
+{
+    class Zamowienie
+    {
+        #region PolaKlasy
+        public DateTime data;
+        public string klient;
+        Sprzedaz[] pozycjeZamowienia;
+        #endregion
+
+        #region Konstruktor
+        public Zamowienie(DateTime data, string klient)
+        {
+            this.data = data;
+            this.klient = klient;
+            pozycjeZamowienia = new Sprzedaz[20];
+        }
+        #endregion
+
+        #region Metody
+        public void dodajPozycje(Sprzedaz produkt, int pozycjaNaZamowieniu)
+        {
+            if(pozycjaNaZamowieniu < 20 && pozycjeZamowienia[pozycjaNaZamowieniu] == null)
+                pozycjeZamowienia[pozycjaNaZamowieniu] = produkt;
+            else
+                Console.WriteLine("ERROR - Nie udało sie dodać produktu {0} do zamówienia na pozycję {1}",produkt.nazwaProduktu, pozycjaNaZamowieniu);
+        }
+
+        public void dodajPozycje(string nazwaProduktu, int ilosc, decimal cena)
+        {
+            Sprzedaz nowyProdukt = new Sprzedaz(nazwaProduktu, ilosc, cena);
+            for (int i = 0; i < pozycjeZamowienia.Length; i++)
+            {
+                if(pozycjeZamowienia[i] == null)
+                {
+                    pozycjeZamowienia[i] = nowyProdukt;
+                    break;
+                }
+            }
+        }
+
+        public void daneZamowienia()
+        {
+            Console.WriteLine($"Klient {klient} w dniu {data.ToString("yyyy.mm.dd")} zakupił: ");
+            foreach (Sprzedaz item in pozycjeZamowienia)
+            {
+                if(item != null)
+                    item.wypisz();
+            }
+            Console.WriteLine();
+        }
+        #endregion
+    }
+}
